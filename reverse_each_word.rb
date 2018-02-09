df72ce79c078831321fc201ade3e7e600f7d1a21
Jennifer Hansen <jenn.leigh.hansen@gmail.com>
2018-02-09 14:23:26 +0000

    automatically backed up by learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,7 +1,7 @@
 def reverse_each_word(sentence)
   reversed_words_array = []
   array = sentence.split
-  array.collect do |word|
+  sentence.collect do |word|
     reversed_words_array << word.reverse
   end
   reversed_words_array.join(" ")
